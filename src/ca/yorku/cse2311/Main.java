9ec106146759931abcaa54aaf44ff1d98a0789c4
MarkyC <mmmarcooo@gmail.com>
2015-01-07 12:56:20 -0500

    Hello World!

---
+++ src/ca/yorku/cse2311/Main.java
@@ -0,0 +1,11 @@
+package ca.yorku.cse2311;
+
+/**
+ * Created by cse93167 on 1/7/2015.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
