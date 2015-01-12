44aff8c71e3ca5c1a5b4f8438e8845ae95d5210d
Tron842 <brodyatto@gmail.com>
2015-01-12 13:39:59 -0500

    Adds StandardBar object

---
+++ src/main/java/ca/yorku/cse2311/tab2pdf/model/StandardBar.java
@@ -0,0 +1,14 @@
+package ca.yorku.cse2311.tab2pdf.model;
+
+/**
+ * Created by Brody on 2015-01-12.
+ */
+public class StandardBar implements IParseable {
+
+    public static final String PARSE_SYMBOL = "|";
+
+    public String getSymbol() {
+        return PARSE_SYMBOL;
+    }
+
+}
