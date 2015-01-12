1c4171ba2f981f3ff14ccfbfdba2aa27763ec3f2
Marco Cirillo <mcirillo@code4arour.com>
2015-01-12 13:11:58 -0500

    Creates IParseable interface; shows example usage

---
+++ src/main/java/ca/yorku/cse2311/tab2pdf/model/IParseable.java
@@ -0,0 +1,10 @@
+package ca.yorku.cse2311.tab2pdf.model;
+
+/**
+ * Created by Marco on 1/12/2015.
+ */
+public interface IParseable {
+
+    public String getSymbol();
+
+}
---
+++ src/main/java/ca/yorku/cse2311/tab2pdf/model/Space.java
@@ -0,0 +1,13 @@
+package ca.yorku.cse2311.tab2pdf.model;
+
+/**
+ * Created by Marco on 1/12/2015.
+ */
+public class Space implements IParseable {
+
+    public static final String PARSE_SYMBOL = "-";
+
+    public String getSymbol() {
+        return PARSE_SYMBOL;
+    }
+}
