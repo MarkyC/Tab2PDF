085989753afc8337aded7fa490a8766e2b35e788
Tron842 <brodyatto@gmail.com>
2015-01-12 13:51:54 -0500

    Added Note Object

--- src/main/java/ca/yorku/cse2311/tab2pdf/model/Note.java
+++ src/main/java/ca/yorku/cse2311/tab2pdf/model/Note.java
@@ -3,5 +3,16 @@ package ca.yorku.cse2311.tab2pdf.model;
 /**
  * Created by Brody on 2015-01-12.
  */
-public class Note {
+public class Note implements IParseable {
+
+    private final String note;
+
+    public Note(String note) {
+        this.note = note;
+    }
+
+    public String getSymbol() {
+        return note;
+    }
+
 }
