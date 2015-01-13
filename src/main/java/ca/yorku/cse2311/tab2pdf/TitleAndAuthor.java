9fc5ab04fed7bea4bf27098536fd381007980266
deepxd <deep_.09@hotmail.com>
2015-01-13 13:46:53 -0500

    is this okay?

--- src/main/java/ca/yorku/cse2311/tab2pdf/TitleAndAuthor.java
+++ src/main/java/ca/yorku/cse2311/tab2pdf/TitleAndAuthor.java
@@ -12,9 +12,14 @@ import com.itextpdf.text.Font;
 import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
+
+import java.awt.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
  
 /**
- * First iText example: Hello World.
+ * Gets the Title and the Author's name from the text file and puts it into a temp pdf.
  */
 public class TitleAndAuthor {
  
@@ -28,10 +33,6 @@ public class TitleAndAuthor {
      */
     public static final String PDF_SUFFIX = ".pdf";
  	
-    /** Path to the resulting PDF file. */
-    public static final String RESULT
-        = "/Users/Deep/Desktop/hello.pdf";
- 
     /**
      * Creates a PDF file: hello.pdf and opens it.
      * @param    args    no arguments needed
@@ -44,7 +45,7 @@ public class TitleAndAuthor {
     		 Path tempFile = Files.createTempFile(FILENAME, PDF_SUFFIX);
     		        
       	    // Run the example code
-           	 new Main().createPdf(tempFile);
+           	 new TitleAndAuthor().createPdf(tempFile);
 
             // open the newly created PDF
              Desktop.getDesktop().open(tempFile.toFile());
@@ -58,7 +59,7 @@ public class TitleAndAuthor {
     }
  
     /**
-     * Creates a PDF document and writes title and authors name.
+     * Creates a TEMP. PDF document and writes title and authors name.
      * @param filename the path to the new PDF document
      * @throws    DocumentException 
      * @throws    IOException 
@@ -73,17 +74,28 @@ public class TitleAndAuthor {
         document.open();
         // step 4
        
-        // Calls method to get the name of the title and author.
-        String TitleAndAuthor = getTitleAndAuthor();
-        String[] TitleAuthor = TitleAndAuthor.split("\n");
+        // creates an empty title and author array list to store title and author.
+        ArrayList<String> lines = new ArrayList<String>();
+        
+        //Opens the user file.
+		File file = new File("/Users/Deep/Desktop/text.txt");
+		
+		//calls the readFile method.
+        lines = TitleAndAuthor.readFile(file);
+        
+        //Calls the getTitle method and store the return variable in title.
+        String title = getTitle(lines);
+        
+        //Calls the getAuthor method and store the return variable in author.
+        String author = getAuthor(lines);
         
         // Setting the font size and alignment of the title
-        Paragraph write_title = new Paragraph(TitleAuthor[0], FontFactory.getFont(FontFactory.HELVETICA, 20, Font.NORMAL));
+        Paragraph write_title = new Paragraph(title, FontFactory.getFont(FontFactory.HELVETICA, 20, Font.NORMAL));
         write_title.setAlignment(Element.ALIGN_CENTER);
         document.add(write_title);
         
         // Setting the font size and alignment of the author
-        Paragraph write_author = new Paragraph(TitleAuthor[1]);
+        Paragraph write_author = new Paragraph(author);
         write_author.setAlignment(Element.ALIGN_CENTER);
         document.add(write_author);
         
@@ -91,35 +103,57 @@ public class TitleAndAuthor {
     }
     
     /**
-     * Opens and reads the text file to extract the tile and the author.
-     * @throws    FileNotFoundException
-     * @throws    IOException 
-     * @return    Author and Title as one string.
+     * Reads the text file stores the first 10 lines in an ArrayList.
+     * @return    ArrayList filled with the lines in the text file.
      */
-    public String getTitleAndAuthor(){
-    	try{
-    		// Opens and reads the file.
-    		File file = new File("/Users/Deep/Desktop/text.txt");
-    		FileReader reading = new FileReader(file);
-    		BufferedReader in = new BufferedReader(reading);
-    		
-    		// Reads the first line.
-    		String Title = in.readLine();
-    		int length = Title.length();
-    		Title = Title.substring(6, length);
-    		
-    		// Reads the second line.
-    		String Author = in.readLine();
-    		length = Author.length();
-		    Author = Author.substring(9, length);
-    		
-		    // returns the just the author and title name.
-    		return Title + '\n' + Author;
-			} catch (FileNotFoundException e){
-				System.err.printf("File not found!" + e.getMessage());
-			} catch (IOException e){
-				System.err.printf("Can not read!" + e.getMessage());
-			}
-		return null;
-}
+    public static ArrayList<String> readFile(File f){
+    	// Opens and reads the file.
+    	FileReader reading = new FileReader(f);
+		BufferedReader in = new BufferedReader(reading);
+		
+		//Creates an empty list.
+	    ArrayList<String> lines = new ArrayList<String>();
+		
+	    //Stores the first 10 lines in ArrayList.
+	    for(int i = 0; i < 10; i++){
+	    	String element = in.readLine();
+	    	lines.add(i, element);
+		}
+	    //Returns the ArrayList.
+	    return lines; 
+    }
+    
+   /**
+    * Extracts the tile from the first line.
+    * @param lines in the ArrayList.
+    * @return Title
+    */
+    public static String getTitle(ArrayList<String> lines){
+    	//Gets the first element in the list which is assumed to be the title
+    	String title = lines.get(0);
+    	
+    	//Extracts just the title.
+    	int length = title.length();
+		title = title.substring(6, length);
+		
+		//Returns title.
+		return title;
+    }
+    
+    /**
+     * Extracts the Authors name from the second line.
+     * @param lines in the ArrayList.
+     * @return Author's name
+     */
+    public static String getAuthor(ArrayList<String> lines){
+    	//Gets the first element in the list which is assumed to be the title
+    	String Author = lines.get(1);
+    	
+    	//Extracts just the title.
+    	int length = Author.length();
+		Author = Author.substring(9, length);
+		
+		//Returns title.
+		return Author;
+    }
 }
