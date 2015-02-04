ddee3c70218bfabf1de3671d12a3f6dbc9ac9d2d
MarkyC <mmmarcooo@gmail.com>
2015-02-03 22:12:52 -0500

    merges in Team 1's PDFHelper, resolves conflicts in Main.

--- src/main/java/ca/yorku/cse2311/tab2pdf/Main.java
+++ src/main/java/ca/yorku/cse2311/tab2pdf/Main.java
@@ -27,103 +27,26 @@ public class Main {
     /**
      * The name of the PDF file we will create
      */
-    public static final String FILENAME = "hello";
+    public static final String FILENAME = "tab2pdf-";
+
     /**
      * PDF file suffix (*.pdf)
      */
     public static final String PDF_SUFFIX = ".pdf";
+
     private final static Logger LOG = Logger.getLogger(Main.class.getName());
-    public static File inputFile;
-    public static File outputFile;
 
     /**
      * Prints program usage and exits
      */
-    public static void printUsageExit() {
+    public static void printUsage() {
 
         System.out.println("Usage: java -jar Tab2Pdf.jar [(-i|--input) <input_file>] [(-o|--output) <output_file>]");
         System.out.println("Example: java -jar Tab2Pdf.jar -i someTab.txt -o somePdf.pdf");
-        System.exit(0);
-
-    }
-
-    public static void parseArgs(String[] args) {
-
-        for (int i = 0; i < args.length; i++) {
-
-            String arg = args[i];
-
-            switch (arg.charAt(0)) {
-
-                case '-':
-
-                    if ((1 < arg.length()) && ('-' == arg.charAt(1))) {
-                        // This is a double-dash option: --input or --output
-
-                        if ("-input".equals(arg.substring(1))) {   // Set the input file
-
-                            setInputFile(args[i + 1]);
-
-
-                        } else if ("-output".equals(arg.substring(1))) {
-
-                            setOutputFile(args[i + 1]);
-
-                        }
-
-                    } else {
-                        if ("i".equals(arg.substring(1))) {   // Set the input file
-
-                            setInputFile(args[i + 1]);
-
-
-                        } else if ("o".equals(arg.substring(1))) {
-
-                            setOutputFile(args[i + 1]);
-
-                        }
-                    }
-                    break;
-
-            }
-        }
-    }
-
-    public static void setInputFile(String arg) {
-
-        try {
-
-            inputFile = new File(arg);
-            FileUtils.testFileRead(inputFile);
-
-        } catch (ArrayIndexOutOfBoundsException ex) {
-            System.out.println("No input file specified.");
-            printUsageExit();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            printUsageExit();
-        }
-
-    }
-
-    public static void setOutputFile(String arg) {
-
-        try {
-
-            outputFile = new File(arg);
-            FileUtils.testFileReadWrite(outputFile);
-
-        } catch (ArrayIndexOutOfBoundsException ex) {
-            System.out.println("No output file specified.");
-            printUsageExit();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            printUsageExit();
-        }
-
     }
 
     private static String findInputFileInArgs(String[] args) throws Exception {
+
         for (int i = 0; i < args.length; i++) {
 
             String arg = args[i];
@@ -159,6 +82,7 @@ public class Main {
     }
 
     private static String findOutputFileInArgs(String[] args) throws Exception {
+
         for (int i = 0; i < args.length; i++) {
 
             String arg = args[i];
@@ -217,10 +141,15 @@ public class Main {
             }
 
             try {
-                arguments.setOutputFile(findOutputFileInArgs(args));
+                arguments.setOutputFile(FileUtils.createTempFile(FILENAME, PDF_SUFFIX));
+                arguments.setOutputFile(findOutputFileInArgs(args));    // if this fails, the output is a temp file
             } catch (Exception e) {
                 LOG.warning(e.getMessage());
             }
+        } else {
+
+            // No Args given? Show them the usage.
+            printUsage();
         }
 
         // This starts our GUI
@@ -241,13 +170,9 @@ public class Main {
         // This creates a PDF in the background using the command line arguments. Our GUI is not wired up yet
         try {
 
-            // we will allow the output file to be null. This means the user just wants to generate a temporary PDF
-            if (null == arguments.getOutputFile()) {
-                outputFile = FileUtils.createTempFile(FILENAME, PDF_SUFFIX);
-            }
 
             // Run the example code
-            new Main().createPdf(arguments.getOutputFile());
+            new Main().createPdf(arguments.getInputFile(), arguments.getOutputFile());
 
             // open the newly created PDF
             Desktop.getDesktop().open(arguments.getOutputFile());
@@ -264,19 +189,20 @@ public class Main {
     /**
      * Creates a PDF document.
      *
-     * @param file the File that will be the new PDF document
+     * @param in  the File that will be the new guitar tab
+     * @param out the File that will be the new PDF document
      * @throws DocumentException
      * @throws IOException
      */
-    public void createPdf(File file) throws Exception {
+    public void createPdf(File in, File out) throws Exception {
 
         // We'll clean this up later! I don't like the overuse of statics here, and everywhere in this file
-        java.util.List<String> lines = FileUtils.readFile(inputFile);
+        java.util.List<String> lines = FileUtils.readFile(in);
 
         // step 1
         Document document = new Document();
         // step 2
-        PdfWriter writer = PdfWriter.getInstance(document, Files.newOutputStream(file.toPath()));
+        PdfWriter writer = PdfWriter.getInstance(document, Files.newOutputStream(out.toPath()));
         // step 3
         document.open();
         // step 4
@@ -286,21 +212,22 @@ public class Main {
         drawShapes(writer);
 
 
-        for (int i = 0; i <= 9; i++) {
+        for (int i = 0; i < 9; i++) {
             stave(i, writer);
-            PdfHelper.drawDigit(3, 1, i * 50, i, writer);
+            PdfHelper.drawDigit(3, 1, 100 * i, i, writer);
         }
-
+        //blankSpace(0, 1, 100, writer);
         // step 5
-
+        PdfHelper.drawDigit(0, 1, 100, 1, writer);
 
         document.close();
     }
 
     private void drawShapes(PdfWriter writer) {
+
         linesCircles(0, 50, writer);
         circlesLines(0, 550, writer);
-        for(int i = 0; i <= 600 ; i += 100){
+        for (int i = 0; i <= 600; i += 100) {
             thinLine(1, i, writer);
             thickLine(2, i, writer);
         }
