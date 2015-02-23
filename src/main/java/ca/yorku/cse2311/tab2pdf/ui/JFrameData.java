4ef55040691d9f923b12e6d7447ff2db376a5ee0
Sitiugin <glib.sitiugin@gmail.com>
2015-02-23 12:54:41 -0500

    Changed App according to profs considerations: - Scaling panel now works - Spacing panel now works
    Also:
    -Put String constants into JFrame

--- src/main/java/ca/yorku/cse2311/tab2pdf/ui/JFrameData.java
+++ src/main/java/ca/yorku/cse2311/tab2pdf/ui/JFrameData.java
@@ -15,22 +15,26 @@ import java.io.Serializable;
  * @since 2015-02-21
  */
 public final class JFrameData {
+
     public static final String EMPTY_FILE_PATH = "Type the file path, or select a file by clicking browse...";
+
     /**
      * Minimum size of the window
      */
     public static final Dimension WINDOW_MIN_SIZE = new Dimension(800, 600);
+
     /**
      * Size of input editor panel
      */
     public static final Dimension EDITOR_PANEL_SIZE = new Dimension(640, 480);
+
     /**
      * Title of the GUI window
      */
     public static final String WINDOW_TITLE = "Tab2PDF";
 
     /**
-     * Default scaling of the Pdf file
+     * Scaling constants of the Pdf file
      */
     public static final int MEDIUM_SCALING = PdfHelper.getLineSpace();
 
@@ -41,6 +45,19 @@ public final class JFrameData {
     public static final int LARGE_SCALING = MEDIUM_SCALING + 2;
 
     public static final int EXTRA_LARGE_SCALING = MEDIUM_SCALING + 4;
+
+    /**
+     * Radio button names
+     */
+    public static final String EXTRA_LARGE_BUTTON_NAME = "Large+";
+
+    public static final String LARGE_BUTTON_NAME = "Large";
+
+    public static final String MEDIUM_BUTTON_NAME = "Medium";
+
+    public static final String SMALL_BUTTON_NAME = "Small";
+
+    public static final String EXTRA_SMALL_BUTTON_NAME = "Small-";
 
     /**
      * Filters *.txt and *.text files
@@ -54,4 +71,33 @@ public final class JFrameData {
      * Filters *.pdf files
      */
     public static final FileFilter PDF_FILE_FILTER = new FileNameExtensionFilter("PDF Files (*.pdf)", "pdf");
+
+    public static final String EDITOR_PANEL_NAME = "Input Editor";
+
+    public static final String IO_PANEL_NAME = "File Selection";
+
+    public static final String CONTROL_PANEL_NAME = "Control Panel";
+
+    public static final String SCALING_PANEL_NAME = "Scaling Panel";
+
+    public static final String SPACING_PANEL_NAME = "Spacing Panel";
+
+    public static final String INPUT_BUTTON_NAME = "Browse For Input File  ";
+
+    public static final String OUTPUT_BUTTON_NAME = "Browse For Output File";
+
+    public static final String CONVERT_BUTTON_NAME = "Convert to Pdf";
+
+    public static final String SAVE_BUTTON_NAME = "Save Changes";
+
+    public static final String EDITOR_TOOLTIP_TEXT = "Edit the input file";
+
+    public static final String IO_TOOLTIP_TEXT = "Select the input and output files for the program";
+
+    public static final String CONVERT_TOOLTIP_TEXT = "Preview the Pdf file or save the changes to the input file";
+
+    public static final String SCALING_TOOLTIP_TEXT = "Select the overall size of the Pdf elements";
+
+    public static final String SPACING_TOOLTIP_TEXT = "Select the horizontal spacing between the Pdf elements";
+
 }
