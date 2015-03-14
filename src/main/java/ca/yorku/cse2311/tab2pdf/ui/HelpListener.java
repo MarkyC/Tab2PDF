b93c30cb663f74d2dca369a5cbe86827b99981fa
Anton Sitkovets <anton@uniswipe.com>
2015-03-14 16:10:53 -0400

    Added a button in the Help frame which links to the User manual. Created method to open the URL and check if the path has correct syntax
    Link to User Manual now works and is displayed through a button in the
    help frame.
    
    Fixed issue of entire app closing when the Help frame was closed.
    
    Refactored the OpenUrlAction class so that you can open any Url, instead
    of only the user manual link.

--- src/main/java/ca/yorku/cse2311/tab2pdf/ui/HelpListener.java
+++ src/main/java/ca/yorku/cse2311/tab2pdf/ui/HelpListener.java
@@ -1,8 +1,15 @@
 package ca.yorku.cse2311.tab2pdf.ui;
 
 import javax.swing.*;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Desktop;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
 import java.util.EventListener;
 import java.util.logging.Level;
 
@@ -10,7 +17,7 @@ import java.util.logging.Level;
  * Created by Glib Sitiugin on 2015-03-06.
  */
 public class HelpListener extends JFrameListener implements EventListener, ActionListener {
-
+	
     /**
      * Constructor
      *
@@ -20,17 +27,34 @@ public class HelpListener extends JFrameListener implements EventListener, Actio
 
         super(frame);
     }
-
+    
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        LOGGER.log(Level.INFO, e.paramString());
-        //TODO: make user manual work
+    	LOGGER.log(Level.INFO, e.paramString());
+        
         // create a new frame with an embedded user manual
-        JFrame helpFrame = new JFrame("User Manual");
+        JFrame helpFrame = new JFrame("Help");
         helpFrame.setSize(500, 500);
-        JLabel label = new JLabel("Will be implemented soon!", JLabel.CENTER);
-        helpFrame.add(label);
+        Container container = helpFrame.getContentPane();
+        container.setLayout(new GridBagLayout());
+        JButton manualButton = new JButton();
+        manualButton.setText("<HTML>User Manual</HTML>");
+        manualButton.setHorizontalAlignment(SwingConstants.LEFT);
+        manualButton.setBorderPainted(false);
+        manualButton.setOpaque(true);
+        manualButton.setBackground(Color.WHITE);
+        manualButton.addActionListener(new OpenUrlAction());
+        
+        
+        
+        
+        container.add(manualButton);
         helpFrame.setVisible(true);
+        
+
+        
     }
+ 
+    
 }
